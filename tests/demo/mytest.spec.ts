5855d85d47617dae6828c840908378e8ec453d0e
Osmangani99 <osmangani360@gmail.com>
2026-08-27 22:24:04 -0400

    locator

--- tests/demo/mytest.spec.ts
+++ tests/demo/mytest.spec.ts
@@ -16,3 +16,20 @@ test ("Should do something",{tag:"@smoke"}, async({page}, testInfo) =>{
  // steps....
  // Code block
 });
+
+// demo locator 
+test.only ("Should demo locator", async({page}, testInfo) =>{
+
+// ✅`page.getBy()` and `page.locator()` methods returns the `locator` object
+// ✅The above methods not to be `awaited`
+// ✅The type of locator is an `object`
+// ✅Locators are LAZY until an action is fired on them
+
+  // 1. Launch URL
+  await page.goto('https://katalon-demo-cura.herokuapp.com/');
+
+  // 2. Click on the Make Appoinment 
+  await page.getByRole('link', { name: 'Make Appointment' }).click();
+  await expect(page.getByText('Please login to make')).toBeVisible();
+
+});
